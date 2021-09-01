8c7fbcb0bd22a6edf5db0d697daa3977fb922b8e
Karel Tutsu <KarelOmab@gmail.com>
2021-09-01 14:36:57 -0400

    MediaInfo queries
    Sample MediaInfo queries

--- MediaExplorer/WindowMain.cs
+++ MediaExplorer/WindowMain.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MediaInfoLib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,160 @@ namespace MediaExplorer
 {
     public partial class WindowMain : Form
     {
+        private string _path;
+        public string Path
+        {
+            get { return _path; }
+            set { _path = value; }
+        }
+        
         public WindowMain()
         {
             InitializeComponent();
+        }
+
+        private void WindowMain_Load(object sender, EventArgs e)
+        {
+            LoadMediaData();
+        }
+
+        private void LoadMediaData()
+        {
+            if (Path == null)
+                Path = TextBoxPath.Text;
+
+            listView1.Items.Clear();
+
+            if (Directory.Exists(Path))
+                foreach(string f in Directory.GetFiles(Path))
+                    HandleFile(f);
+
+            if (listView1.Items.Count > 0)
+                listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+
+        private void HandleFile(string f)
+        {
+            ListViewItem lvi = new ListViewItem();
+            lvi.Text = System.IO.Path.GetFileName(f);
+
+            ExampleQuery(f);
+            ExampleWithStream(f);
+
+
+
+            listView1.Items.Add(lvi);
+        }
+
+        private void ExampleQuery(string f)
+        {
+            //Test if version of DLL is compatible : 3rd argument is "version of DLL tested;Your application name;Your application version"
+            String ToDisplay;
+            MediaInfo MI = new MediaInfo();
+
+            ToDisplay = MI.Option("Info_Version", "0.7.0.0;MediaInfoDLL_Example_CS;0.7.0.0");
+            if (ToDisplay.Length == 0)
+            {
+                MessageBox.Show("MediaInfo.Dll: this version of the DLL is not compatible");
+                return;
+            }
+
+            //Information about MediaInfo
+            ToDisplay += "\r\n\r\nInfo_Parameters\r\n";
+            ToDisplay += MI.Option("Info_Parameters");
+
+            ToDisplay += "\r\n\r\nInfo_Capacities\r\n";
+            ToDisplay += MI.Option("Info_Capacities");
+
+            ToDisplay += "\r\n\r\nInfo_Codecs\r\n";
+            ToDisplay += MI.Option("Info_Codecs");
+
+            //An example of how to use the library
+            ToDisplay += "\r\n\r\nOpen\r\n";
+            MI.Open(f);
+
+            ToDisplay += "\r\n\r\nInform with Complete=false\r\n";
+            MI.Option("Complete");
+            ToDisplay += MI.Inform();
+
+            ToDisplay += "\r\n\r\nInform with Complete=true\r\n";
+            MI.Option("Complete", "1");
+            ToDisplay += MI.Inform();
+
+            ToDisplay += "\r\n\r\nCustom Inform\r\n";
+            MI.Option("Inform", "General;File size is %FileSize% bytes");
+            ToDisplay += MI.Inform();
+
+            ToDisplay += "\r\n\r\nGet with Stream=General and Parameter='FileSize'\r\n";
+            ToDisplay += MI.Get(0, 0, "FileSize");
+
+            ToDisplay += "\r\n\r\nGet with Stream=General and Parameter=46\r\n";
+            ToDisplay += MI.Get(0, 0, 46);
+
+            ToDisplay += "\r\n\r\nCount_Get with StreamKind=Stream_Audio\r\n";
+            ToDisplay += MI.Count_Get(StreamKind.Audio);
+
+            ToDisplay += "\r\n\r\nGet with Stream=General and Parameter='AudioCount'\r\n";
+            ToDisplay += MI.Get(StreamKind.General, 0, "AudioCount");
+
+            ToDisplay += "\r\n\r\nGet with Stream=Audio and Parameter='StreamCount'\r\n";
+            ToDisplay += MI.Get(StreamKind.Audio, 0, "StreamCount");
+
+            ToDisplay += "\r\n\r\nClose\r\n";
+            MI.Close();
+
+            //Example with a stream
+            //ToDisplay+="\r\n"+ExampleWithStream()+"\r\n";
+
+            //Displaying the text
+            Console.WriteLine(ToDisplay);
+            //richTextBox1.Text = ToDisplay;
+        }
+
+        String ExampleWithStream(string f)
+        {
+            //Initilaizing MediaInfo
+            MediaInfo MI = new MediaInfo();
+
+            //From: preparing an example file for reading
+            FileStream From = new FileStream(f, FileMode.Open, FileAccess.Read);
+
+            //From: preparing a memory buffer for reading
+            byte[] From_Buffer = new byte[64 * 1024];
+            int From_Buffer_Size; //The size of the read file buffer
+
+            //Preparing to fill MediaInfo with a buffer
+            MI.Open_Buffer_Init(From.Length, 0);
+
+            //The parsing loop
+            do
+            {
+                //Reading data somewhere, do what you want for this.
+                From_Buffer_Size = From.Read(From_Buffer, 0, 64 * 1024);
+
+                //Sending the buffer to MediaInfo
+                System.Runtime.InteropServices.GCHandle GC = System.Runtime.InteropServices.GCHandle.Alloc(From_Buffer, System.Runtime.InteropServices.GCHandleType.Pinned);
+                IntPtr From_Buffer_IntPtr = GC.AddrOfPinnedObject();
+                Status Result = (Status)MI.Open_Buffer_Continue(From_Buffer_IntPtr, (IntPtr)From_Buffer_Size);
+                GC.Free();
+                if ((Result & Status.Finalized) == Status.Finalized)
+                    break;
+
+                //Testing if MediaInfo request to go elsewhere
+                if (MI.Open_Buffer_Continue_GoTo_Get() != -1)
+                {
+                    Int64 Position = From.Seek(MI.Open_Buffer_Continue_GoTo_Get(), SeekOrigin.Begin); //Position the file
+                    MI.Open_Buffer_Init(From.Length, Position); //Informing MediaInfo we have seek
+                }
+            }
+            while (From_Buffer_Size > 0);
+
+            //Finalizing
+            MI.Open_Buffer_Finalize(); //This is the end of the stream, MediaInfo must finnish some work
+
+            //Get() example
+            Console.WriteLine("Container format is " + MI.Get(StreamKind.General, 0, "Format"));
+            return "Container format is " + MI.Get(StreamKind.General, 0, "Format");
         }
     }
 }
